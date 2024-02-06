ce808dbc313bcffd3a731f5d2290dd6c9f226457
ChiomaE <chioma.eriken@gmail.com>
2024-02-06 09:14:58 -0600

    word problem complete

--- index.js
+++ index.js
@@ -10,24 +10,24 @@ let n= [10,15,20,25];
 // This is a fairly simple operation using
 // arithmetic operators and a comparison.
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
-
+console.log(isSum50);
 // Check two: at least two odd numbers
 // Here, we use modulus to check if something is odd.
 // Since % 2 is 0 if even and 1 if odd, we can use
 // arithmetic to count the total number of odd numbers.
 const isTwoOdd = (n1 % 2) + (n2 % 2) + (n3 % 2) + (n4 % 2) >= 2;
-
+console.log(isTwoOdd);
 // Check three: no number larger than 25
 // This time, we use the OR operator to check
 // if ANY of the numbers is larger than 25.
 const isOver25 = n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25;
-
+console.log(isOver25);
 // Check four: all unique numbers
 // This is long, and there are more efficient
 // ways of handling it with other data structures
 // that we will review later.
 const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4;
-
+console.log(isUnique);
 // Here, we put the results into a single variable 
 // for convenience. Note how we negate isOver25 using
 // the ! operator. We could also have tested for 
@@ -46,13 +46,14 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
   !(n1 > 25 || n2 > 25 || n3 > 25 || n4 > 25) && 
   (n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 != n4);
 
-
+  console.log(dontDoThis);
 // Check if all numbers are divisible by 5. Cache the result in a variable.
 const divisible = (n1%5) + (n2%5) + (n3%5) + (n4%5) === 0;
 console.log("Is divisible is " + divisible);
 
 // Check if the first number is larger than the last. Cache the result in a variable.
 const lastLarge = n1 > n4;
+console.log(lastLarge);
 
 
 
@@ -72,11 +73,11 @@ Change the way that isOver25 calculates so that we do not need to use the NOT op
 
 const isUnder25 = n1 < 25 || n2 < 25 || n3 < 25 || n4 < 25;
 
-/* const isValid = isSum50 && isTwoOdd && isUnder25 && isUnique;
+const isValid = isSum50 && isTwoOdd && isUnder25 && isUnique;
 
 console.log(isValid);
- */
-
+ 
+// over25 as a loop
 let over25 = false;
 
 for (let i = 0; i < 5; i++) {
---
+++ index2.js
@@ -0,0 +1,69 @@
+/* 
+You are planning a cross-country road trip!
+The distance of the trip, in total, is 1,500 miles.
+Your car’s fuel efficiency is as follows:
+At 55 miles per hour, you get 30 miles per gallon.
+At 60 miles per hour, you get 28 miles per gallon.
+At 75 miles per hour, you get 23 miles per gallon.
+You have a fuel budget of $175.
+The average cost of fuel is $3 per gallon.
+Set up a program to answer the following questions:
+How many gallons of fuel will you need for the entire trip?
+Will your budget be enough to cover the fuel expense?
+How long will the trip take, in hours?
+Compare the results when traveling at an average of 55, 60, and 75 miles per hour. Which makes the most sense for the trip?
+Log the results of your calculations using string concatenation or template literals. 
+*/
+
+const totalDistance = 1500;
+let budget = 175;
+costOfFuel = 3;
+
+//
+/* const mph5 = 55;
+
+let totalGallon = totalDistance/mph5;
+console.log(`${totalGallon} gallons`);
+
+let totalCost = totalGallon/costOfFuel;
+console.log(totalCost);
+
+let budgetAfter = budget - totalCost;
+console.log(`Your budget after fuel is $${budgetAfter}`);
+
+let travelTime = totalDistance/55;
+console.log(travelTime.toFixed() + " hours"); */
+
+/* 
+const mph6 = 60; 
+
+let totalGallon = totalDistance/mph6;
+console.log(`${totalGallon} gallons`);
+
+let totalCost = totalGallon/costOfFuel;
+console.log(totalCost);
+
+let budgetAfter = budget - totalCost;
+console.log(`Your budget after fuel is $${budgetAfter}`);
+
+let travelTime = totalDistance/55;
+console.log(travelTime.toFixed() + " hours");
+ */
+
+/* 
+const mph7 = 75; 
+
+let totalGallon = totalDistance/mph7;
+console.log(`${totalGallon} gallons`);
+
+let totalCost = totalGallon/costOfFuel;
+console.log(totalCost);
+
+let budgetAfter = budget - totalCost;
+console.log(`Your budget after fuel is $${budgetAfter}`);
+
+let travelTime = totalDistance/55;
+console.log(travelTime.toFixed() + " hours");
+ */
+
+console.log("75mph is the most cost effective for this trip");
