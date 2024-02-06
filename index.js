cd7ffa0b8d0dc08b66aaf0b1d6c327b3f007272f
ChiomaE <chioma.eriken@gmail.com>
2024-02-06 08:41:47 -0600

    code finish 1

--- index.js
+++ index.js
@@ -49,6 +49,7 @@ const dontDoThis = ((n1 + n2 + n3 + n4) == 50) &&
 
 // Check if all numbers are divisible by 5. Cache the result in a variable.
 const divisible = (n1%5) + (n2%5) + (n3%5) + (n4%5) === 0;
+console.log("Is divisible is " + divisible);
 
 // Check if the first number is larger than the last. Cache the result in a variable.
 const lastLarge = n1 > n4;
